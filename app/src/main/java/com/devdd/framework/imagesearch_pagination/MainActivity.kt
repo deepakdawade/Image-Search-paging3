3a07be0d458158852300ad5161ef424687c88bab
deepak <deepakdawade.dd@gmail.com>
2020-09-24 22:39:24 +0530

    Initial commit

---
+++ app/src/main/java/com/devdd/framework/imagesearch_pagination/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.devdd.framework.imagesearch_pagination
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
