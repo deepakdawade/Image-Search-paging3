e6099b4f8010ef75eafb46f8c839a4c4630876ea
deepak <deepakdawade.dd@gmail.com>
2020-09-26 00:27:48 +0530

    Android Entry Point

---
+++ app/src/main/java/com/devdd/framework/imagesearch_pagination/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,27 @@
+package com.devdd.framework.imagesearch_pagination.ui.gallery
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.*
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.devdd.framework.imagesearch_pagination.data.UnSplashPhoto
+import com.devdd.framework.imagesearch_pagination.data.repository.UnsplashRepository
+
+/**
+ * Created by @author Deepak Dawade on 9/25/2020 at 10:15 PM.
+ * Copyright (c) 2020 deepakdawade.dd@gmail.com All rights reserved.
+ **/
+class GalleryViewModel @ViewModelInject constructor(
+    private val repository: UnsplashRepository
+):ViewModel() {
+    companion object{
+        private const val DEFAULT_QUERY = "cats"
+    }
+    private val currentQuery:MutableLiveData<String> = MutableLiveData()
+    val photos: LiveData<PagingData<UnSplashPhoto>> = currentQuery.switchMap {
+        repository.getSearchResults(it).cachedIn(viewModelScope)
+    }
+    fun searchPhotos(query:String){
+        currentQuery.value = query
+    }
+}
