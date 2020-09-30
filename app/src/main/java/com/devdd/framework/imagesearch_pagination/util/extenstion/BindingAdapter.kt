1fe3728a1b3b3ef7a732025939be06330556d32c
deepak <deepakdawade.dd@gmail.com>
2020-09-30 23:34:34 +0530

    BindingAdapter.kt

--- app/src/main/java/com/devdd/framework/imagesearch_pagination/util/extenstion/BindingAdapter.kt
+++ app/src/main/java/com/devdd/framework/imagesearch_pagination/util/extenstion/BindingAdapter.kt
@@ -2,7 +2,6 @@ package com.devdd.framework.imagesearch_pagination.util.extenstion
 
 import android.graphics.drawable.Drawable
 import android.util.Log
-import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -14,19 +13,19 @@ import com.bumptech.glide.request.RequestOptions
  * Copyright (c) 2020 deepakdawade.dd@gmail.com All rights reserved.
  **/
 
-internal fun <T> AppCompatImageView.setImage(url: T) {
+internal fun <T> AppCompatImageView.loadImage(url: T) {
     try {
         Glide.with(this.context)
             .load(url)
             .fitCenter()
             .into(this)
     } catch (e: IllegalArgumentException) {
-        Log.e("BindingAdapter", "setImage: ",e )
+        Log.e("BindingAdapter", "loadImage: ", e)
     }
 }
 
 @BindingAdapter("app:setRoundImage", "app:placeholderForRoundImage")
-fun AppCompatImageView.setRoundImage(url: String?, placeholder: Drawable) {
+fun AppCompatImageView.roundImage(url: String?, placeholder: Drawable) {
     Glide.with(this.context)
         .load(url)
         .placeholder(placeholder)
@@ -35,19 +34,20 @@ fun AppCompatImageView.setRoundImage(url: String?, placeholder: Drawable) {
 }
 
 @BindingAdapter("loadImage", "errorPlaceholder", requireAll = false)
-fun AppCompatImageView.glideLoadImage(
+fun AppCompatImageView.loadImage(
     imagePath: String?,
     errorPlaceHolder: Drawable? = null
 ) {
     val glideRequest = Glide.with(this)
-    if (imagePath != null) {
+    imagePath?.let { url ->
         val loader =
-            glideRequest.load(imagePath).centerCrop().transition(DrawableTransitionOptions.withCrossFade())
+            glideRequest.load(url).centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade(100))
         errorPlaceHolder?.let { drawable ->
             loader.placeholder(drawable)
         }
         loader.into(this)
-    } else {
+    } ?: run {
         if (errorPlaceHolder != null) {
             val loader = glideRequest.load(errorPlaceHolder)
                 .transition(DrawableTransitionOptions.withCrossFade(100))
