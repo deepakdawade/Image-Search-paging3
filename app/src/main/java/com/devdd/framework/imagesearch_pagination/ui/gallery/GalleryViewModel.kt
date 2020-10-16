8d44bdc54ef101616d25f6b74e1f5f9a2822c8f6
Deepak Dawade <deepakdawade.dd@gmail.com>
2020-10-16 22:20:22 +0530

    process death

--- app/src/main/java/com/devdd/framework/imagesearch_pagination/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/devdd/framework/imagesearch_pagination/ui/gallery/GalleryViewModel.kt
@@ -1,5 +1,6 @@
 package com.devdd.framework.imagesearch_pagination.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
 import androidx.paging.PagingData
@@ -12,16 +13,23 @@ import com.devdd.framework.imagesearch_pagination.data.repository.UnsplashReposi
  * Copyright (c) 2020 deepakdawade.dd@gmail.com All rights reserved.
  **/
 class GalleryViewModel @ViewModelInject constructor(
-    private val repository: UnsplashRepository
-):ViewModel() {
-    companion object{
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
+) : ViewModel() {
+    companion object {
         private const val DEFAULT_QUERY = "cats"
+        private const val CURRENT_QUERY = "current_query"
     }
-    private val currentQuery:MutableLiveData<String> = MutableLiveData(DEFAULT_QUERY)
+
+    private val currentQuery: MutableLiveData<String> = state.getLiveData(
+        CURRENT_QUERY,
+        DEFAULT_QUERY
+    )
     val photos: LiveData<PagingData<UnSplashPhoto>> = currentQuery.switchMap {
         repository.getSearchResults(it).cachedIn(viewModelScope)
     }
-    fun searchPhotos(query:String){
+
+    fun searchPhotos(query: String) {
         currentQuery.value = query
     }
 }
